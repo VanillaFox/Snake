b78389ca2d84d77acc6839c1fedd52bd476af731
VanillaFox <71337244+VanillaFox@users.noreply.github.com>
2021-04-03 23:29:41 +0300

    class Game and class Map

---
+++ game.h
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "fruit.h"
+#include "snake.h"
+#include <unistd.h>
+#include <ctime>
+
+class Map{
+    private:
+        char Wall = '+';
+        char STail = '*';
+        char SHead = 'o';
+        char EFruit = '$';
+        int Wedht, Height;
+        std::clock_t Start;
+
+        bool Same(Snake &snake, Fruit &fruit);
+        void TimeFruit(Snake &snake, Fruit &fruit);
+        void Horizontal();
+        void DrawMap(Snake &snake, Fruit &fruit);
+        void MiddleMap(Snake &snake, Fruit &fruit);
+        
+    public:
+        Map();
+        ~Map();
+
+        void NewSides(int &w, int &h);
+        void Check(Snake &snake, Fruit &fruit);
+        void Draw(Snake &snake, Fruit &fruit);
+};
+
+class Game{
+    private:
+        enum Directon {Stop = 0, Left, Right, Up, Down, End};
+
+        Map map;
+        Snake snake;
+        Fruit fruit;
+        int height, wedht, Score = 0;
+        bool GameOver;
+        Directon dir;
+
+        void Draw();
+        void EatFruit();
+        void Move();
+        void Input();
+
+    public:
+        void Play();
+        Game(int a = 30, int b = 20): height(b), wedht(a), GameOver(false), dir(Stop){
+            map.NewSides(a, b);
+            snake.NewHead(a, b);
+            fruit.NewSide(a, b);
+            fruit.AddFruit();
+        }
+
+        ~Game(){
+            std::cout << std::endl;
+            std::cout << "Game Over!\n";
+        }
+};
