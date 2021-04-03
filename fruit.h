94c41564f65138a4b96f4e672dd722d4e748ee1a
VanillaFox <71337244+VanillaFox@users.noreply.github.com>
2021-04-03 23:26:41 +0300

    Update fruit.h

--- fruit.h
+++ fruit.h
@@ -3,18 +3,24 @@
 class Fruit{
     private:
         std::vector<std::pair<int, int>> Fruits;
+        int Wedht, Height;
+        int maxSize;
     public:
-        Fruit();
+        Fruit(int i = 3): maxSize(i){}
         ~Fruit();
         
-        int FruitsSize();
+        int Size();
+        int MaxSize();
         int FruitX(int i);
         int FruitY(int i);
         int LastFruitX();
         int LastFruitY();
 
-
         void AddFruit();
         void RewriteFruit(int i);
         void NewCoordsForLast();
+        void NewSide(int w, int h){
+            Wedht = w;
+            Height = h;
+        }
 };
